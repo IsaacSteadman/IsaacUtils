e8e4a7dfa59a47e2685ac0d2a684e7e48a4da58e
IsaacSteadman <isaac.steadman@yahoo.com>
2015-10-18 00:53:30 -0500

    Added HashMap templates and hopefully everything that they need except for the hash function

--- IsaacUtils/TemplateUtils.h
+++ IsaacUtils/TemplateUtils.h
@@ -503,4 +503,219 @@ namespace Utils{
 	T &IterArray<T>::operator*(){
 		return (*Str)[Pos];
 	}
+	template<typename T1, typename T2>
+	class HashMap {
+	public:
+		typedef unsigned long(*HashFunc)(T1 &, unsigned long);
+		struct DataPair {
+			T1 Key;
+			T2 Val;
+			DataPair *Next;
+			bool IsValid;
+			DataPair();
+			DataPair(T1 KeyVal, T2 Value, DataPair *next = 0);
+			DataPair(const T1 &KeyVal, const T2 &Value, DataPair *next = 0);
+		};
+	private:
+		HashFunc Hasher;
+		DataPair *Datas;
+		unsigned long AllocNum;
+		//returns false if the key holder is in the root list: pointer points to it, returns true if not: pointer points to previous key holder in linked list
+		bool GetWithKey(const T1 &KeyVal, DataPair *&PtrPrev);
+	public:
+		HashMap(HashFunc hasher = 0);
+		T2 &AtKey(const T1 &KeyVal);
+		const T2 Get(const T1 &KeyVal) const;
+		void Put(const T1 &KeyVal, const T2 &Value);
+		bool Rem(const T1 &KeyVal);
+		void PreAlloc(unsigned long Num);
+		unsigned long NumHashes();
+		const T1 *FindKey(const T2 &Value) const;
+		Array<DataPair *> GetKeyPairs() const;
+	};
+	template<typename T1, typename T2>
+	HashMap<T1, T2>::DataPair::DataPair() {
+		Next = 0;
+		IsValid = false;
+	}
+	template<typename T1, typename T2>
+	HashMap<T1, T2>::DataPair::DataPair(const T1 &KeyVal, const T2 &Value, DataPair *next = 0) {
+		Key = KeyVal;
+		Val = Value;
+		Next = next;
+		IsValid = true;
+	}
+	template<typename T1, typename T2>
+	HashMap<T1, T2>::DataPair::DataPair(T1 KeyVal, T2 Value, DataPair *next = 0) {
+		Key = KeyVal;
+		Val = Value;
+		Next = next;
+		IsValid = true;
+	}
+	template<typename T1, typename T2>
+	HashMap<T1, T2>::HashMap(HashFunc hasher = 0) {
+		Hasher = hasher;
+		AllocNum = 8;
+		Datas = new DataPair[AllocNum];
+	}
+	template<typename T1, typename T2>
+	T2 &HashMap<T1, T2>::AtKey(const T1 &KeyVal) {
+		DataPair *GetPair = 0;
+		bool Tmp = GetWithKey(KeyVal, GetPair);
+		if (Tmp) GetPair = GetPair->Next;
+		return GetPair->Val;
+	}
+	template<typename T1, typename T2>
+	const T2 HashMap<T1, T2>::Get(const T1 &KeyVal) const {
+		DataPair *GetPair = 0;
+		bool Tmp = GetWithKey(KeyVal, GetPair);
+		if (Tmp) GetPair = GetPair->Next;
+		return GetPair->Val;
+	}
+	template<typename T1, typename T2>
+	void HashMap<T1, T2>::Put(const T1 &KeyVal, const T2 &Value) {
+		DataPair *GetPair = 0;
+		bool Tmp = GetWithKey(KeyVal, GetPair);
+		if (GetPair != 0)
+		{
+			if (Tmp) GetPair = GetPair->Next;
+			GetPair->Val = Value;
+		}
+		else
+		{
+			unsigned long Pos = Hasher(KeyVal, AllocNum);
+			GetPair = Datas[Pos];
+			if (Datas[Pos].IsValid) GetPair = Datas[Pos]->Next;
+			else
+			{
+				Datas[Pos].Key = KeyVal;
+				Datas[Pos].Val = Value;
+				Datas[Pos].IsValid = true;
+				return;
+			}
+			while (GetPair->Next != 0)
+				GetPair = GetPair->Next;
+			GetPair->Next = new DataPair(KeyVal, Value);
+		}
+	}
+	template<typename T1, typename T2>
+	bool HashMap<T1, T2>::Rem(const T1 &KeyVal) {
+		DataPair *GetPair = 0;
+		bool Tmp = GetWithKey(KeyVal, GetPair);
+		if (Tmp)
+		{
+			DataPair * OrigNext = GetPair->Next;
+			GetPair->Next = OrigNext->Next;
+			delete OrigNext;
+		}
+		else
+		{
+			if (GetPair->Next == 0)
+			{
+				GetPair->Key = T1();
+				GetPair->Val = T2();
+				GetPair->IsValid = false;
+			}
+			else
+			{
+				DataPair OrigNext = GetPair->Next;
+				GetPair->Next = OrigNext->Next;
+				//we are basically moving from OrigNext to GetPair so to be efficient we will use move assignment
+				GetPair->Key.operator=((T1 &&)OrigNext->Key);
+				GetPair->Val.operator=((T2 &&)OrigNext->Val);
+				//since we MOVED from OrigNext, deleting OrigNext shouldn't invalidate GetPair's copies of Key and Val
+				delete OrigNext;
+			}
+		}
+	}
+	template<typename T1, typename T2>
+	void HashMap<T1, T2>::PreAlloc(unsigned long Num) {
+		//basically this is rehashing the entire map with a different number of hashes
+		DataPair *NewDatas = new DataPair[Num];
+		Array<DataPair *> OrigPairs = (Array<DataPair *>)GetKeyPairs();
+		for (DataPair *&Pair : OrigPairs) {
+			unsigned long Pos = Hasher(Pair->Key, Num);
+			DataPair *NewPair = &NewDatas[Pos];
+			if (NewDatas[Pos].IsValid)
+			{
+				while (NewPair->Next != 0) NewPair = NewPair->Next;
+				NewPair->Next = new DataPair();
+				NewPair = NewPair->Next;
+			}
+			NewPair->Key.operator=((T1 &&)Pair->Key);
+			NewPair->Val.operator=((T2 &&)Pair->Val);
+			NewPair->IsValid = true;
+		}
+		//deallocate the pairs that not directly in the array of pairs
+		unsigned long c = OrigPairs.Length();
+		while (c > 0)
+		{
+			--c;
+			if ((unsigned long)(OrigPairs[c] - Datas) >= AllocNum)
+			{
+				delete OrigPairs[c];
+			}
+		}
+		//deallocate the array of pairs
+		delete[] Datas;
+		//set the array of pairs to the new array
+		AllocNum = Num;
+		Datas = NewDatas;
+	}
+	template<typename T1, typename T2>
+	unsigned long HashMap<T1, T2>::NumHashes() {
+		unsigned long Rtn = 0;
+		for (unsigned long c = 0; c < AllocNum; ++c) if (Datas[c].IsValid) ++Rtn;
+		return Rtn;
+	}
+	template<typename T1, typename T2>
+	const T1 *HashMap<T1, T2>::FindKey(const T2 &Value) const {
+		for (const DataPair *Pair : GetKeyPairs()) {
+			if (Pair->Val == Value) return &Pair->Key;
+		}
+		return 0;
+	}
+	template<typename T1, typename T2>
+	Array<typename HashMap<T1, T2>::DataPair *> HashMap<T1, T2>::GetKeyPairs() const {
+		unsigned long NumDats = 0;
+		Array<DataPair *> DataPairs;
+		for (unsigned long c = 0; c < AllocNum; ++c) {
+			if (!Datas[c].IsValid) continue;
+			++NumDats;
+			DataPair *Cur = &Datas[c];
+			while (Cur->Next != 0) {
+				Cur = Cur->Next;
+				++NumDats;
+			}
+		}
+		DataPairs.SetLength(NumDats);
+		IterArray<DataPair *> IterPairs = DataPairs.begin();
+		for (unsigned long c = 0; c < AllocNum; ++c) {
+			if (!Datas[c].IsValid) continue;
+			*IterPairs = Datas[c];
+			++IterPairs;
+			DataPair *Cur = &Datas[c];
+			while (Cur->Next != 0) {
+				Cur = Cur->Next;
+				*IterPairs = Cur;
+				++IterPairs;
+			}
+		}
+		return DataPairs;
+	}
+	template<typename T1, typename T2>
+	bool HashMap<T1, T2>::GetWithKey(const T1 &KeyVal, DataPair *&PtrPrev) {
+		unsigned long Pos = Hasher(KeyVal, AllocNum);
+		PtrPrev = 0;
+		if (!Datas[Pos].IsValid) return false;
+		PtrPrev = Datas[Pos];
+		if (Datas[Pos].Key == KeyVal) return false;
+		while (PtrPrev->Next != 0) {
+			DataPair *Cur = PtrPrev->Next;
+			if (Cur->Key == KeyVal) return true;
+			PtrPrev = Cur;
+		}
+		PtrPrev = 0;
+		return false;
+	}
 }
